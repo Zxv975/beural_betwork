ed692e27157c263fef4b2cc5fd4d54a2e6f6197e
Rek <53563546+Rek2@users.noreply.github.com>
2019-11-04 14:46:31 +1100

    aaaaa

--- ricki network.py
+++ ricki network.py
@@ -1,10 +1,7 @@
 import network_modded
 import mnist_loader
+import numpy as np
 training_data, validation_data, test_data = mnist_loader.load_data_wrapper()
 
 net = network_modded.Network([784,30,10])
 net.SGD(training_data, 1, 10, 3.0, test_data=test_data)
-
-from tempfile import TemporaryFile
-outfile = TemporaryFile()
-np.save(outfile, net.biases)
