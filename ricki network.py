5341bf103bf304fa1779ad665b574caa28a57bcd
Rek <53563546+Rek2@users.noreply.github.com>
2019-11-04 18:51:24 +1100

    added json saving for net weights, biases, size and layer data

--- ricki network.py
+++ ricki network.py
@@ -1,10 +1,42 @@
 import network_modded
 import mnist_loader
+import json
+import numpy as np
 training_data, validation_data, test_data = mnist_loader.load_data_wrapper()
+
+def net_encode(net):
+    # Convert numpy arrays to lists to be serialised
+    bias_data = [b.tolist() for b in net.biases]
+    weight_data = [w.tolist() for w in net.weights]
+    layer_data = net.num_layers
+    size_data = net.sizes
+
+    # Generate an array of data and serialise it
+    return json.dumps([bias_data, weight_data, layer_data, size_data])
+
+def net_decode(data):
+    # Deserialise the data
+    json_list = json.loads(data)
+
+    # Convert the weights and biases back into numpy arrays
+    my_bias = [np.asarray(b) for b in json_list[0]]
+    my_weights = [np.asarray(w) for w in json_list[1]]
+
+    return [my_bias, my_weights, json_list[2], json_list[3]]
 
 net = network_modded.Network([784,30,10])
 net.SGD(training_data, 1, 10, 3.0, test_data=test_data)
 
-with open("trainedVariables.txt", "w") as text_file:
-    print(f"Weights: \n {net.weights}", file=text_file)
-    print(f"\nBiases: \n {net.biases}", file=text_file)
+data = net_encode(net)
+
+# Write to file
+f = open("network_data.txt", "w")
+f.write(data)
+f.close()
+
+# Read from file
+f = open("network_data.txt", "r")
+my_data = net_decode(f.read())
+f.close()
+
+print (my_data)
